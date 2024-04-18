3cc657e1697b12283a7f14b86fa5e73261e5a223
Umarkhon <kudratkhujaev@bk.ru>
2024-04-18 16:49:57 +0500

    js

---
+++ index.js
@@ -0,0 +1,21 @@
+const years = 2024
+const birthYears = [2000, 2001, 2002, 2003]
+const newYear = []
+
+
+
+    // for(let year of birthYears){
+    //     console.log( years - year );
+    // }
+
+    function calcAge() {
+        for (let year of birthYears){
+            newYear.push( years - year)
+        }
+            
+    }
+    calcAge()
+
+console.log(newYear )
+
+
